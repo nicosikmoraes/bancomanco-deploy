ff5466907ff49cad1d6253f62cb6f73687446701
nicosikmoraes <nicolas.moraes.ns@gmail.com>
2025-05-14 16:54:01 -0300

    Deposito funcion, container direita

--- src/app/banco-page/banco-page.component.ts
+++ src/app/banco-page/banco-page.component.ts
@@ -2,16 +2,24 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ShareDataService } from '../services/share-data.service';
 import { ClienteService } from '../services/cliente.service';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { NgxMaskDirective, provideNgxMask } from 'ngx-mask';
 
 @Component({
   selector: 'app-banco-page',
-  imports: [],
+  imports: [FormsModule,
+            CommonModule,
+            NgxMaskDirective,
+  ],
+    providers: [provideNgxMask()],
   templateUrl: './banco-page.component.html',
   styleUrl: './banco-page.component.css'
 })
 export class BancoPageComponent implements OnInit{
   clienteNome!: string | undefined;
   dados: any;
+  valorDeposito: number | null = null;
 
   constructor( 
       private shareDataService: ShareDataService,
@@ -48,6 +56,14 @@ export class BancoPageComponent implements OnInit{
         alert("Sem saldo suficiente")
       }
   }
+
+  deposito(){
+    this.dados.saldo = this.dados.saldo + this.valorDeposito;
+    console.log(this.dados.saldo, "valorDeposto: ", this.valorDeposito);
+    this.updateDatabase();
+
+    this.valorDeposito = null;
+  };
 
 
 }
